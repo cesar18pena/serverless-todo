9ce08bd7cc80488860701b169e89f7d4149d433e
cesar18pena <cesar18pena@gmail.com>
2020-06-14 19:22:00 -0400

    Delete complete and fixed create

--- backend/src/lambda/http/createTodo.ts
+++ backend/src/lambda/http/createTodo.ts
@@ -1,80 +1,38 @@
 import "source-map-support/register";
+import * as middy from "middy";
+import { cors } from "middy/middlewares";
 import {
-  APIGatewayProxyHandler,
   APIGatewayProxyEvent,
+  APIGatewayProxyHandler,
   APIGatewayProxyResult,
 } from "aws-lambda";
-import * as AWS from "aws-sdk";
-import * as uuid from "uuid";
+import { CreateTodoRequest } from "../../requests/CreateTodoRequest";
+import { createTodo } from "../../businessLogic/";
 import { createLogger } from "../../utils/logger";
-import { parseUserId } from "../../auth/utils";
 
-const docClient = new AWS.DynamoDB.DocumentClient();
-
-const todosTable = process.env.TODOS_TABLE;
-
-export const handler: APIGatewayProxyHandler = async (
+const createHandler: APIGatewayProxyHandler = async (
   event: APIGatewayProxyEvent
 ): Promise<APIGatewayProxyResult> => {
+  // TODO: Implement creating a new TODO item
+
   const logger = createLogger("CreateTodo");
   logger.info("Event data:", event);
 
-  const todoId = uuid.v4();
-  const jsonBody = JSON.parse(event.body);
+  const newTodo: CreateTodoRequest = JSON.parse(event.body);
+  const authorization = event.headers.Authorization;
+  const split = authorization.split(" ");
+  const jwtToken = split[1];
 
-  const authHeader = event.headers.Authorization;
-  const authSplit = authHeader.split(" ");
-  const token = authSplit[1];
-
-  const item = {
-    todoId: todoId,
-    userId: parseUserId(token),
-    ...jsonBody,
-  };
-
-  await docClient
-    .put({
-      TableName: todosTable,
-      Item: item,
-    })
-    .promise();
-
+  const newItem = await createTodo(newTodo, jwtToken);
   return {
     statusCode: 201,
     headers: {
       "Access-Control-Allow-Origin": "*",
     },
     body: JSON.stringify({
-      item,
+      newItem,
     }),
   };
 };
 
-// import 'source-map-support/register'
-
-// import { APIGatewayProxyEvent, APIGatewayProxyHandler, APIGatewayProxyResult } from 'aws-lambda'
-
-// import { CreateTodoRequest } from '../../requests/CreateTodoRequest'
-// import { getUserId } from '../utils'
-// import { createLogger } from '../../utils/logger'
-// import { createTodo } from '../../logicLayer/todo'
-// const logger = createLogger('Todo')
-
-// export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
-//   const userId = getUserId(event);
-//   logger.info('auth user id ', userId)
-//   logger.info('Processing event: ', event);
-//   const newTodo: CreateTodoRequest = JSON.parse(event.body);
-
-//   // TODO: Implement creating a new TODO item
-
-//   const todoItem = await createTodo(newTodo, userId)
-//   return {
-//     statusCode: 201,
-//     headers: {
-//       'Access-Control-Allow-Origin': '*',
-//       'Access-Control-Allow-Credentials': true
-//     },
-//     body: JSON.stringify({item: todoItem})
-//   }
-// }
+export const handler = middy(createHandler).use(cors({ credentials: true }));
--- backend/src/lambda/http/deleteTodo.ts
+++ backend/src/lambda/http/deleteTodo.ts
@@ -1,38 +1,35 @@
 import "source-map-support/register";
+import * as middy from "middy";
+import { cors } from "middy/middlewares";
 import {
-  APIGatewayProxyHandler,
   APIGatewayProxyEvent,
+  APIGatewayProxyHandler,
   APIGatewayProxyResult,
 } from "aws-lambda";
-import * as AWS from "aws-sdk";
+import { deleteTodo } from "../../businessLogic/";
 import { createLogger } from "../../utils/logger";
 
-const docClient = new AWS.DynamoDB.DocumentClient();
-
-const todosTable = process.env.TODOS_TABLE;
-
-export const handler: APIGatewayProxyHandler = async (
+const deleteHandler: APIGatewayProxyHandler = async (
   event: APIGatewayProxyEvent
 ): Promise<APIGatewayProxyResult> => {
+  // TODO: Remove a TODO item by id
+
   const logger = createLogger("DeleteTodo");
   logger.info("Event data:", event);
 
   const todoId = event.pathParameters.todoId;
+  const authorization = event.headers.Authorization;
+  const split = authorization.split(" ");
+  const jwtToken = split[1];
 
-  await docClient
-    .delete({
-      TableName: todosTable,
-      Key: {
-        todoId: todoId,
-      },
-    })
-    .promise();
-
+  await deleteTodo(todoId, jwtToken);
   return {
-    statusCode: 201,
+    statusCode: 204,
     headers: {
       "Access-Control-Allow-Origin": "*",
     },
-    body: "Item removed",
+    body: "Item deleted successfully",
   };
 };
+
+export const handler = middy(deleteHandler).use(cors({ credentials: true }));
